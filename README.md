4977fd1fa46b92595f6fd42c705cabb96ada0f86
Alvaro Guzman <alvaro.guzlop@gmail.com>
2015-06-20 20:10:48 +0200

    Probar tipos php7

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # catar-php7
 Unos simples script para probar el nuevo php7
+
+http://pushthebutton.es
+
+(Lo de catar viene del [verbo|https://github.com/alwar/catar-php7.git], no tiene nada que ver con la ciudad ni con las katas japos)
---
+++ Seven.php
@@ -0,0 +1,70 @@
+<?php
+namespace Catar;
+
+error_reporting(0);
+echo "error_reporting(E_ALL);\n";
+
+class Dummy {
+	public function __toString() {
+		return "no soy nadie... snif \n";
+	}
+}
+
+class Seven {
+
+    public function helloWorld():string
+    {
+        return "Hola Mundo\n";
+    }
+
+    public function fuckReturnTypes():array
+    {
+    	$obj = new Dummy();
+    	return $obj;
+    }
+
+	public function whatAboutNull():Dummy {
+		return null;
+	}
+
+	public function whatAboutCasting():int {
+		return 'A';
+	}
+
+	public function andIfITellYouAnotherCasting():string {
+		return  -69.283;
+	}
+}
+
+$nuevoPhp = new Seven();
+
+echo $nuevoPhp->helloWorld();
+
+try {
+	echo $nuevoPhp->fuckReturnTypes();
+} catch(\EngineException $e) {
+	echo "\n\n{$e->getMessage()}\n";
+}
+
+try {
+	echo $nuevoPhp->whatAboutNull();
+} catch(\EngineException $e) {
+	echo "\n{$e->getMessage()}\n";
+	
+}
+
+try {
+	echo $nuevoPhp->whatAboutCasting();
+} catch(\EngineException $e) {
+	echo "\n{$e->getMessage()}\n";
+	
+}
+
+try {
+	echo "\n";
+	echo $nuevoPhp->andIfITellYouAnotherCasting();
+	echo "\n";
+} catch(\EngineException $e) {
+	echo "\n{$e->getMessage()}\n";
+	
+}
